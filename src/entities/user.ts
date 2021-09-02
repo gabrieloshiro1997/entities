adddd2cf846dd16c3c2f2649f3bd5db7ad8e7831
Gabriel Oshiro <gabrieloshiro1997@gmail.com>
2021-09-02 20:57:07 -0300

    feat: add user entity

---
+++ src/entities/user.ts
@@ -0,0 +1,23 @@
+import { Column, Entity, OneToMany } from 'typeorm'
+import { BaseEntity } from './base'
+import { Role } from './role'
+
+@Entity('user')
+export class User extends BaseEntity {
+  @Column({ type: 'varchar' })
+  name: string
+
+  @Column({ type: 'varchar', unique: true })
+  email: string
+
+  @Column({ type: 'varchar', nullable: true })
+  password?: string
+
+  @OneToMany((_) => Role, (role) => role.user, { eager: true })
+  rolePermissions: Role[]
+
+  constructor (partial: Partial<User>) {
+    super()
+    Object.assign(this, partial)
+  }
+}
