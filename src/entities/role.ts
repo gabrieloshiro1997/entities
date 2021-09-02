59978cf9cf291aa56523d8bfe623e9c0d90f8e7d
Gabriel Oshiro <gabrieloshiro1997@gmail.com>
2021-09-02 20:57:19 -0300

    feat: add role entity

---
+++ src/entities/role.ts
@@ -0,0 +1,23 @@
+import { Column, Entity, ManyToOne } from 'typeorm'
+import { BaseEntity } from './base'
+import { User } from './user'
+
+@Entity('roles')
+export class Role extends BaseEntity {
+  @Column({ type: 'varchar' })
+  name: string
+
+  @Column({ type: 'varchar' })
+  description: string
+
+  @Column({ type: 'uuid' })
+  userId: string
+
+  @ManyToOne((_) => User, (user) => user.rolePermissions, { nullable: true })
+  user: User
+
+  constructor (partial: Partial<Role>) {
+    super()
+    Object.assign(this, partial)
+  }
+}
